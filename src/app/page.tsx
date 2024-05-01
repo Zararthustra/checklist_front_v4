1b0f23baaf3a552223494b2738464bb8a2eb6165
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-05-01 16:52:39 +0200

    feat(Database): setup database & server component log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
   const urls = [
     "https://utfs.io/f/dd0e64e1-38ab-491b-b914-56eed433e3bc-y0ghj0.jpg",
     "https://utfs.io/f/b3f2e8d7-225d-46c1-948b-7453a04b475b-jlo1ag.jpg",
     "https://utfs.io/f/f81d063f-cd81-4438-8673-449ad233b490-wvkjh8.jpg",
     "https://utfs.io/f/80280395-1120-44eb-8825-166a9d22b836-wvkjgd.jpg",
   ];
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1>Checklist</h1>

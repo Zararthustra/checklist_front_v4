60c568ffa3a5fb320b9a5e25dbd8bf37a4b5a885
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-05-02 20:39:13 +0200

    feat(Delete): add delete management

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,4 +1,4 @@
-import { getMyImages } from "~/server/queries";
+import { deleteImage, getMyImages } from "~/server/queries";
 import UploadButtons from "./UploadButtons";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,12 +10,22 @@ export default async function Images() {
     <div>
       <div className="flex items-center justify-center gap-2">
         {images.map((img) => (
-          <Link key={img.id} href={`img/${img.id}`}>
-            <div className="relative h-48 w-48">
-              <Image src={img.url} fill alt={img.name} sizes="192px" />
-            </div>
-            <p>{img.name}</p>
-          </Link>
+          <>
+            <Link key={img.id} href={`img/${img.id}`}>
+              <div className="relative h-48 w-48">
+                <Image src={img.url} fill alt={img.name} sizes="192px" />
+              </div>
+              <p>{img.name}</p>
+            </Link>
+            <form
+              action={async () => {
+                "use server";
+                await deleteImage(img.id);
+              }}
+            >
+              <button>Delete</button>
+            </form>
+          </>
         ))}
       </div>
       <UploadButtons />

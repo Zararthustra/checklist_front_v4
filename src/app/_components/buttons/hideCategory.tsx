66e7ad221de9718429bb32ae105ae2948b5f2767
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2024-05-07 16:47:26 +0200

    feat(Loading): add loading states

--- src/app/_components/buttons/hideCategory.tsx
+++ src/app/_components/buttons/hideCategory.tsx
@@ -1,24 +1,34 @@
 "use client";
 
 import { toast } from "sonner";
-import { IconHidden, IconVisible } from "~/app/_assets/icons";
+import { IconHidden, IconLoader, IconVisible } from "~/app/_assets/icons";
 import { ICategory } from "~/app/_interfaces";
 import { updateCategory } from "~/server/queries";
+import { useState } from "react";
 
 export default function HideCategory({ category }: { category: ICategory }) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   return (
     <p
       className="cursor-pointer text-2xl font-bold"
       onClick={async () => {
+        setIsLoading(true);
         const { error } = await updateCategory(
           category.id,
           "isHidden",
           !category.isHidden,
         );
+        setIsLoading(false);
         if (error) toast.error(error);
       }}
     >
-      {category.isHidden ? <IconHidden /> : <IconVisible />}
+      {isLoading ? (
+        <IconLoader />
+      ) : category.isHidden ? (
+        <IconHidden />
+      ) : (
+        <IconVisible />
+      )}
     </p>
   );
 }
--- src/app/_components/task.tsx
+++ src/app/_components/task.tsx
@@ -3,7 +3,8 @@
 import { toast } from "sonner";
 import { delTask, updateTask } from "~/server/queries";
 import { ICategory, ITask } from "../_interfaces";
-import { IconCheck, IconCheckEmpty } from "../_assets/icons";
+import { IconCheck, IconCheckEmpty, IconLoader } from "../_assets/icons";
+import { useState } from "react";
 
 export default function Task({
   task,
@@ -12,14 +13,20 @@ export default function Task({
   task: ITask;
   category: ICategory;
 }) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const checkTask = async () => {
+    setIsLoading(true);
     const { error, data } = await updateTask(task);
+    setIsLoading(false);
     if (error) toast.error(error);
     else toast.success(data);
   };
 
   const removeTask = async () => {
+    setIsLoading(true);
     const { error, data } = await delTask(task);
+    setIsLoading(false);
     if (error) toast.error(error);
     else toast.success(data);
   };
@@ -36,15 +43,21 @@ export default function Task({
         color:
           category.isRecurrent && task.isDisabled ? category.textColor : "",
       }}
-      className="flex cursor-pointer items-center bg-white py-1 text-center dark:bg-black"
+      className="flex cursor-pointer items-center justify-center bg-white py-1 text-center dark:bg-black"
     >
-      {category.isRecurrent &&
-        (task.isDisabled ? (
-          <IconCheck className="ml-4" />
-        ) : (
-          <IconCheckEmpty className="ml-4" />
-        ))}
-      <p className="w-full">{task.name}</p>
+      {isLoading ? (
+        <IconLoader />
+      ) : (
+        <>
+          {category.isRecurrent &&
+            (task.isDisabled ? (
+              <IconCheck className="ml-4" />
+            ) : (
+              <IconCheckEmpty className="ml-4" />
+            ))}
+          <p className="w-full">{task.name}</p>
+        </>
+      )}
     </div>
   );
 }
